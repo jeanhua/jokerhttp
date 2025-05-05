ba43e3da6a03cd87cab369b717ef53e1d44dc39e
jeanhua <2207739460@qq.com>
2025-05-05 13:08:03 +0800

    测试

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"time"
 
 	"github.com/jeanhua/jokerhttp"
 	"github.com/jeanhua/jokerhttp/engine"
@@ -16,14 +17,14 @@ func main() {
 	joker.SetPort(1314)
 
 	// 添加日志中间件
-	joker.Use(func(ctx *engine.Contex) {
+	joker.Use(func(ctx *engine.JokerContex) {
 		println("Request received:", ctx.Request.URL.Path)
 		ctx.Next()
 		println("Response sent")
 	})
 
 	// 添加认证中间件
-	joker.Use(func(ctx *engine.Contex) {
+	joker.Use(func(ctx *engine.JokerContex) {
 		if ctx.Request.Header.Get("Authorization") != "secret" {
 			ctx.AbortWithStatusJSON(401, map[string]string{"error": "Unauthorized"})
 			ctx.Abort()
@@ -42,6 +43,20 @@ func main() {
 
 	joker.MapPost("/echo", func(r *http.Request, b []byte, p url.Values) (int, interface{}) {
 		return 200, map[string]string{"original": string(b)}
+	})
+
+	joker.MapGet("/cache", func(request *http.Request, params url.Values) (status int, response interface{}) {
+		// 获取缓存值
+		value, found := joker.Cache.TryGet("myKey")
+		if found {
+			return 200, map[string]interface{}{"value": value}
+		}
+		// 设置缓存值
+		minutes := time.Now().Minute()
+		hours := time.Now().Hour()
+		seconds := time.Now().Second()
+		joker.Cache.Set("myKey", map[string]interface{}{"time": fmt.Sprintf("%02d:%02d:%02d", hours, minutes, seconds)}, joker.Cache.AbsoluteTimeFromNow(time.Second*60)) // 60秒后过期
+		return 200, map[string]interface{}{"message": "Cache set!"}
 	})
 
 	// 静态文件服务
