b1675d1a634f01bf49e1ca59cc3fc41ea912d581
jeanhua <2207739460@qq.com>
2025-05-08 10:49:41 +0800

    test router

--- test/Router_test.go
+++ test/Router_test.go
@@ -18,22 +18,21 @@ func TestRouter(t *testing.T) {
 		ctx.ResponseWriter.Header().Add("middleware", "root")
 		ctx.Next()
 	})
-	api := root.Group("/api")
-	api.Use(func(ctx *engine.JokerContex) {
-		ctx.ResponseWriter.Header().Add("middleware", "api")
+	api1 := root.Group("/api1")
+	api2 := root.Group("/api2")
+	api1.Use(func(ctx *engine.JokerContex) {
+		ctx.ResponseWriter.Header().Add("middleware", "api1")
 		ctx.Next()
 	})
-	api.Map("/test", func(request *http.Request, params url.Values, setHeaders func(key, value string)) (status int, response interface{}) {
-		setHeaders("Content-Type", "application/json")
-		return 200, map[string]interface{}{
-			"code":    0,
-			"message": "ok",
-			"data": map[string]interface{}{
-				"method": request.Method,
-				"path":   request.URL.Path,
-				"query":  params,
-			},
-		}
+	api2.Use(func(ctx *engine.JokerContex) {
+		ctx.ResponseWriter.Header().Add("middleware", "api2")
+		ctx.Next()
+	})
+	api1.Map("/test", func(request *http.Request, params url.Values, setHeaders func(key, value string)) (status int, response interface{}) {
+		return 200, "api1 test"
+	})
+	api2.Map("/test", func(request *http.Request, params url.Values, setHeaders func(key, value string)) (status int, response interface{}) {
+		return 200, "api2 test"
 	})
 	joker.Run()
 }
